79f9d1255f67651eaff18b0ee50dadeb69148f6f
urlaaswani <35735799+urlaaswani@users.noreply.github.com>
2018-01-24 11:55:52 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: false,
+  PIXABAY_API_KEY:'7742318-e2acd7c32afd80a58f68d24c8',
+  PIXABAY_API_URL:'https://pixabay.com/api/?key='
+};
 };
